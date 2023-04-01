3fa87d260b29017236d434d9e29844cdee51291f
AnuragM87 <anuragchaudharym87@gmail.com>
2023-04-01 10:36:11 +0530

    my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  // const list=[1,2,3];
+  const number1=5;
+  const number2=4;
+  const sum=number1+number2
+
+  return (
+    <div className="App">
+ <p>first app</p>
+ <p>{sum}</p>
+    </div>
+  );
+}
+
+export default App;
